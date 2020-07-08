de0a80ef5af4fab60a3073ba07b183cecc5a072f
Ubuntu <ubuntu@ip-172-31-17-103.ap-southeast-1.compute.internal>
2020-07-08 11:24:16 +0000

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         @stack('meta')
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <title>KỲ SMILE BLOG</title>
         <link rel="icon" href="img/favicon.png">
         <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
